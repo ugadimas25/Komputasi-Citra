19ea028f09206fa442a4e2ff00de336683c06484
ugadimas25 <45408028+ugadimas25@users.noreply.github.com>
2021-04-05 08:11:00 +0700

    Add files via upload

---
+++ 1_Komputasi Citra.js
@@ -0,0 +1,24 @@
+// 03 KOMPUTASI CITRA
+
+// Cari 'dsm' (model permukaan digital) dan temukan:
+var dsm = ee.Image('JAXA/ALOS/AW3D30_V1_1');
+
+// Dapatkan ketinggian dalam pita saluran meter.
+var elev = dsm.select('AVE');
+
+// Perhitungan ini ('junk') hanyalah contoh matematika dengan citra.
+var junk = elev.add(3);
+Map.addLayer(junk, { min: 0, max: 500 }, 'junk');
+
+// Operasi relasional pada citra. Temukan semua tempat
+// ketinggian lebih dari 500 meter. Citra biner.
+var elevGt500 = elev.gt(500);
+// Ini adalah trik yang berguna. Setel semua nol piksel menjadi 'no data' (masked).
+elevGt500 = elevGt500.updateMask(elevGt500);
+Map.addLayer(elevGt500, { palette: ['yellow'] }, 'elevGt500');
+
+// Gunakan metode statis untuk perhitungan yang lebih kompleks.
+var terrain = ee.Terrain.products(elev);
+// Cetak untuk mengetahui band apa yang ada di sana.
+print('terrain', terrain);
+Map.addLayer(terrain, { bands: ['hillshade'] }, 'hillshade');
